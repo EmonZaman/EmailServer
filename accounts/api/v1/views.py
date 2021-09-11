051fa6979591624c1e0ce32321eb4acd5580b961
EmonZaman <aagontukemon@gmail.com>
2021-09-11 20:58:21 +0600

    token  genereted

--- accounts/api/v1/views.py
+++ accounts/api/v1/views.py
@@ -1,4 +1,5 @@
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -17,6 +18,9 @@ class UserView(APIView):
         return Response(serializer.data)
 
     def post(self, request, format=None):
+
+        authentication_classes = [TokenAuthentication]
+        permission_classes = (IsAuthenticated,)
         serializer = UserSerailizers(data=request.data)
         if serializer.is_valid():
             serializer.save()
